f38001f2a9eb89a730d319753b5414a2c536ec5e
keeten84 <58552964@qq.com>
2018-09-13 06:03:08 +0800

    apps setup done

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# 搜索目录到配置：如果所有app都放在一个apps下的话，需要添加下列语句，将apps放到搜索路径中
+sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
